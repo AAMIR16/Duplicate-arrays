efa041fe105c618af0169aaa43b094947d113edf
Aamir Abbas <aamir.abbas16@gmail.com>
2020-09-26 13:52:42 -0700

    Duplicates in an array

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // counting # of duplicate vlues
+            int ctr = 0;
+            int[] a = new int[] { 5, 7, 3, 5, 7, 1, 3};
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j= i+1;  j<a.Length; j++)
+                {
+                    if (a[i] != a[j])
+                    {
+                        ctr++;
+                    }
+                }
+            }
+            Console.WriteLine("# of duplicate values are: " +ctr);
+            Console.ReadLine();
+            
+        }
+    }
+}
